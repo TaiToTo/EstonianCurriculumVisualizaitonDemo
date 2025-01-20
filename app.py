6d8614672d3750da22ca8df114778a0d47cb1972
YasuThompson <yasutotamura6161@gmail.com>
2025-01-20 14:01:57 +0100

    Separated graphs as different graph objects and added legend

--- app.py
+++ app.py
@@ -72,6 +72,24 @@ def query_vector(query_text):
     return df_queried
 
 
+def get_rag_answer_and_sources(question, limit=2):
+    class_name = "CurriculumDemo"
+        # Connect to Weaviate Cloud
+    client = weaviate.connect_to_weaviate_cloud(
+        cluster_url=weaviate_url,                                    # Replace with your Weaviate Cloud URL
+        auth_credentials=Auth.api_key(weaviate_api_key),             # Replace with your Weaviate Cloud key
+        headers={"X-Cohere-Api-Key": cohere_api_key},           # Replace with your Cohere API key
+    )
+
+    collection = client.collections.get("CurriculumDemo")
+
+    response = collection.generate.near_text(
+        query="biology",
+        limit=5,
+        grouped_task="Write a tweet with emojis about these facts."
+    )
+
+
 # Load data
 base_path = os.path.dirname(__file__)
 df = pd.read_csv(os.path.join(base_path, "data/embedding_2d_est_basic_school.csv"))
@@ -80,30 +98,21 @@ df = pd.read_csv(os.path.join(base_path, "data/embedding_2d_est_basic_school.csv
 color_list = [subject_color_map[row["subject"]] for _, row in df.iterrows()]
 hover_texts = [row["text"].replace("\n", "<br>") for _, row in df.iterrows()]
 
-# # Create the scatter plot
-# scatter_fig = go.Figure()
-
-# # Add legend items for each subject
-# for subject, color in subject_color_map.items():
-#     scatter_fig.add_trace(go.Scatter(
-#         x=[None], y=[None],  # Dummy points for legend
-#         mode='markers',
-#         marker=dict(size=10, color=color),
-#         name=subject
-#     ))
-
 st.title("Estonian Basic School Curriculum Analyzer Demo")
 
 # Create the subplots with an additional row for the bottom graph
-fig = make_subplots(
-    rows=2,  # Increase the rows to 2
+fig_1 = make_subplots(
+    rows=1,  # Increase the rows to 2
     cols=2,  # Keep the columns as 2 for the top row
-    column_widths=[0.2, 0.8],  # Set column widths for the first row
-    row_heights=[0.6, 0.4],  # Adjust row heights (optional)
-    subplot_titles=("The most relevant texts", "Searched texts grouped by subjects", "Semantic map of texts"),  # Titles for all subplots
+    column_widths=[0.3, 0.7],  # Set column widths for the first row
+    subplot_titles=(
+        "The most relevant texts", 
+        "Searched texts grouped by subjects", 
+        # "Semantic map of texts"
+        ),  # Titles for all subplots
     specs=[
         [{"type": "xy"}, {"type": "domain"}],  # First row specs
-        [{"colspan": 2}, None],  # Second row spans both columns
+        # [{"colspan": 2}, None],  # Second row spans both columns
     ]
 )
 
@@ -116,7 +125,7 @@ if query_text:
 
 
     # Add bar plot to the first column
-    fig.add_trace(
+    fig_1.add_trace(
         go.Bar(
             # y=df_queried['text'].map(lambda x: x),
             y=[idx for idx in range(len(df_queried))],
@@ -129,7 +138,8 @@ if query_text:
         col=1
     )
 
-    fig.update_layout(
+
+    fig_1.update_layout(
         xaxis=dict(
             title='Relavance Score',  # Label for the x-axis
             range=[0.5, 0.8]
@@ -146,7 +156,7 @@ if query_text:
     colors = [subject_color_map[cat] for cat in unique_subject_list + df_queried['subject'].tolist()]
 
     # Add treemap to the second column
-    fig.add_trace(
+    fig_1.add_trace(
         go.Treemap(
             labels=unique_subject_list + df_queried["text"].tolist(),
             parents=[""]*len(unique_subject_list) + df_queried["subject"].tolist(),  # Since "path" isn't directly supported, set parents to empty or adjust accordingly.
@@ -159,7 +169,7 @@ if query_text:
 
 else:
     # Add bar plot to the first column
-    fig.add_trace(
+    fig_1.add_trace(
         go.Bar(
         ),
         row=1,
@@ -167,17 +177,30 @@ else:
     )
 
     # Add treemap to the second column
-    fig.add_trace(
+    fig_1.add_trace(
         go.Treemap(
         ),
         row=1,
         col=2
     )
 
-
+# Create the subplots with an additional row for the bottom graph
+fig_2 = make_subplots(
+    rows=1,  # Increase the rows to 2
+    cols=1,  # Keep the columns as 2 for the top row
+    subplot_titles=(
+        # "The most relevant texts", 
+        # "Searched texts grouped by subjects", 
+        "Semantic map of texts"
+        ),  # Titles for all subplots
+    # specs=[
+    #     [{"type": "xy"}, {"type": "domain"}],  # First row specs
+    #     [{"colspan": 2}, None],  # Second row spans both columns
+    # ]
+)
 
 # Add a line plot to the third subplot (row 2, col 1, spanning both columns)
-fig.add_trace(
+fig_2.add_trace(
     go.Scatter(
         x=df["x"],
         y=df["y"],
@@ -191,12 +214,24 @@ fig.add_trace(
         hoverinfo='text',  # Display only the hover text
         name="Scatter Points", 
     ),
-    row=2,
+    row=1,
     col=1
 )
 
-fig.update_layout(showlegend=False)
-fig.update_layout(height=1000, width=1500)
+# Add legend items for each subject
+for subject, color in subject_color_map.items():
+    fig_2.add_trace(go.Scatter(
+        x=[None], y=[None],  # Dummy points for legend
+        mode='markers',
+        marker=dict(size=10, color=color),
+        name=subject
+    ))
+
+# fig.update_layout(showlegend=False)
+# fig.update_layout(height=1000, width=1500)
 
 # Streamlit app
-st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig_1, use_container_width=True)
+
+st.plotly_chart(fig_2, use_container_width=True)
+
