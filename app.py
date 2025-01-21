9f0f37356989b264d971e87d8d1a2ba93422c687
YasuThompson <yasutotamura6161@gmail.com>
2025-01-21 13:04:51 +0100

    Added a RAG part

--- app.py
+++ app.py
@@ -42,6 +42,8 @@ def query_vectors(query_text, search_limit):
         return_metadata=MetadataQuery(distance=True, certainty=True)
     )
 
+    client.close()  # Free up resources
+
     row_dict_list = []
     for obj in response.objects:
         row_dict = {}
@@ -53,9 +55,8 @@ def query_vectors(query_text, search_limit):
         
         row_dict_list.append(row_dict)
 
-    client.close()  # Free up resources
-
     df_queried = pd.DataFrame(row_dict_list, columns=["text", "subject", "certainty", "distance", "paragraph_idx"])
+    
     df_queried["text"] = df_queried["text"].map(lambda x: x.replace("\n", "<br>") )
 
     df_queried["hover_text"] = df_queried.apply(
@@ -80,14 +81,33 @@ def get_rag_answer_and_sources(query_text, task_text, limit=2):
     response = collection.generate.near_text(
         query=query_text,
         limit=5,
-        grouped_task=task_text
+        grouped_task=task_text, 
+        return_metadata=MetadataQuery(distance=True, certainty=True)
     )
 
-    reference_text_list = [obj.properties["text"] for obj in response.objects]
+    client.close()  # Free up resources
+
+    row_dict_list = []
+    for obj in response.objects:
+        row_dict = {}
+        row_dict["text"] = obj.properties["text"]
+        row_dict["subject"] = obj.properties["subject"]
+        row_dict["paragraph_idx"] = int(obj.properties["paragraph_idx"])
+        row_dict["certainty"] = obj.metadata.certainty
+        row_dict["distance"] = obj.metadata.distance
+        
+        row_dict_list.append(row_dict)
+
+    df_queried = pd.DataFrame(row_dict_list, columns=["text", "subject", "certainty", "distance", "paragraph_idx"])
+    df_queried["text"] = df_queried["text"].map(lambda x: x.replace("\n", "<br>") )
+    df_queried["hover_text"] = df_queried.apply(
+        lambda x: "<b>Relevance:</b> {:.3f}<br><b>Paragraph idx:</b> {}<br><b>Paragraph idx:</b> {}<br>{}".format(x["certainty"], x["subject"], x["paragraph_idx"], x["text"]),
+        axis=1
+    )
 
     generated_text = response.generated
 
-    return generated_text, reference_text_list
+    return generated_text, df_queried
 
 
 st.set_page_config(layout="wide")
@@ -101,7 +121,7 @@ df = pd.read_csv(os.path.join(base_path, "data/embedding_2d_est_basic_school.csv
 color_list = [subject_color_map[row["subject"]] for _, row in df.iterrows()]
 hover_texts = [row["text"].replace("\n", "<br>") for _, row in df.iterrows()]
 
-st.title("Estonian Basic School Curriculum Analyzer Demo")
+st.title("Estonian School Curriculum Analyzer Demo")
 
 # Create the subplots with an additional row for the bottom graph
 fig_1 = make_subplots(
@@ -117,13 +137,12 @@ fig_1 = make_subplots(
     ]
 )
 
-st.subheader("1. Search curriculum")
+st.header("Search curriculum")
 
 query_text = st.text_input("Write a query to search contents of curriculum:", "")
 
-# Add a slider for filtering an integer value
 text_search_limit = st.slider(
-    "Select an integer value",  # Label for the slider
+    "Select a number of texts to query",  # Label for the slider
     min_value=0,               # Minimum value of the slider
     max_value=100,             # Maximum value of the slider
     value=30,                  # Default value
@@ -165,7 +184,6 @@ if query_text:
     )
 
     unique_subject_list = df_queried["subject"].unique().tolist()
-
     colors = [subject_color_map[cat] for cat in unique_subject_list + df_queried['subject'].tolist()]
 
     # Add treemap to the second column
@@ -182,19 +200,15 @@ if query_text:
         col=2
     )
 
-        # Adjust the figure layout for height
     fig_1.update_layout(
         height=750,  # Set the height of the figure (increase as needed)
-        # uniformtext=dict(
-        #         minsize=12,  # Increase font size for the text inside the treemap
-        #         mode="show"
-        #     )
         hoverlabel=dict(
             align="left"  # Left-align the hover text
         )
     )
 
 else:
+    pass
     # Add bar plot to the first column
     fig_1.add_trace(
         go.Bar(
@@ -211,18 +225,81 @@ else:
         col=2
     )
 
-fig_2 = px.scatter(
+# Streamlit app
+st.plotly_chart(fig_1, use_container_width=True)
+
+st.header("Ask question of curriuclum saved")
+rag_search_limit = st.slider(
+    "Select a number of texts to use for generating the answer",  # Label for the slider
+    min_value=0,               # Minimum value of the slider
+    max_value=10,             # Maximum value of the slider
+    value=1,                  # Default value
+    step=1                     # Step size
+)
+# Input fields for two texts
+query_text = st.text_input("Write a query to find reference texts", placeholder="Type something here...")
+task_text = st.text_input("Write a task based on the texts queried:", placeholder="Type something here...")
+
+# Button to combine texts
+if st.button("Make a prompt"):
+    if query_text and task_text:
+        # Combine the texts and output the result
+        generated_text, df_ref = get_rag_answer_and_sources(query_text, task_text, limit=rag_search_limit)
+
+        st.markdown("### Generated answer:")
+        st.write(generated_text)
+
+        unique_subject_list = df_ref["subject"].unique().tolist()
+        colors = [subject_color_map[cat] for cat in unique_subject_list + df_ref['subject'].tolist()]
+
+        fig_2 = go.Figure()
+
+        # Add treemap to the second column
+        fig_2.add_trace(
+            go.Treemap(
+                labels=unique_subject_list + df_ref["hover_text"].tolist(),
+                parents=[""]*len(unique_subject_list) + df_ref["subject"].tolist(),  # Since "path" isn't directly supported, set parents to empty or adjust accordingly.
+                values=[0]*len(unique_subject_list) + df_ref["certainty"].tolist(),
+                marker=dict(colors=colors), 
+                hovertext=unique_subject_list + df_ref['hover_text'].tolist(), 
+                hoverinfo='text' 
+            ),
+
+        )
+
+        fig_2.update_layout(
+            height=150,  # Set the height of the figure (increase as needed)
+            hoverlabel=dict(
+                align="left"  # Left-align the hover text
+            ), 
+            margin=dict(
+                l=10,  # Left margin
+                r=10,  # Right margin
+                t=10,  # Top margin
+                b=10   # Bottom margin
+            )
+        )
+
+        st.markdown("### Queried texts used for generating the answer:")
+        st.plotly_chart(fig_2, use_container_width=True)
+
+    else:
+        st.warning("Please provide both texts to combine.")
+
+
+
+fig_3 = px.scatter(
     df, 
     x="x", 
     y="y", 
     color="subject",  # Color by the subject column
     color_discrete_map=subject_color_map,  # Apply the custom color mapping
     hover_data=['text'],  # Replace with your hover text column name
-    title="Semantic Map of Texts in the Database"
+    title="Semantic map of texts in the database"
 )
 
 # Remove x and y axes labels
-fig_2.update_layout(
+fig_3.update_layout(
     xaxis_title="",  # Set x-axis label to an empty string
     yaxis_title="",   # Set y-axis label to an empty string
     xaxis=dict(showticklabels=False),  # Remove x-axis tick labels
@@ -233,26 +310,7 @@ fig_2.update_layout(
         )
 )
 
-# Streamlit app
-st.plotly_chart(fig_1, use_container_width=True)
 
-st.subheader("2. Ask question of curriuclum saved")
-
-# Input fields for two texts
-query_text = st.text_input("Write a query to find reference texts", placeholder="Type something here...")
-task_text = st.text_input("Write a task based on the texts queried:", placeholder="Type something here...")
-
-# Button to combine texts
-if st.button("Make a prompt"):
-    if query_text and task_text:
-        # Combine the texts and output the result
-        generated_text, ref_list = get_rag_answer_and_sources(query_text, task_text, limit=2)
-
-        st.subheader("Generated Answer:")
-        st.write(generated_text)
-    else:
-        st.warning("Please provide both texts to combine.")
-
-st.subheader("3. Intermediate results for interdisciplinary analysis")
-st.plotly_chart(fig_2, use_container_width=True)
+st.header("Intermediate results for interdisciplinary analysis")
+st.plotly_chart(fig_3, use_container_width=True)
 
